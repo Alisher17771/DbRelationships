41d8f38526940f439f7d7ed70ac4460581919e36
Alisher <Alisher@KOMPUTER>
2023-11-05 22:04:37 +0300

    Добавьте файлы проекта.

---
+++ Models/Character.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BacpackId { get; set; }
+        public Backpack Backpack { get; set; }
+    }
+}
